8e6ebd6afcdc91a505a44a288fd48fd87ba99e6f
LeonardoSya <2246866774@qq.com>
2023-10-18 11:46:44 +0800

    modified:   Interface&Type.ts

--- Interface&Type.ts
+++ Interface&Type.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: LeonardoSya 2246866774@qq.com
- * @Date: 2023-08-13 12:40:32
- * @LastEditors: LeonardoSya 2246866774@qq.com
- * @LastEditTime: 2023-08-14 10:01:30
- * @FilePath: \TypeScript\Interface&Type.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 // Interfaces用来【定义对象的数据类型】
 
 // Interfaces 是对行为的抽象，而具体如何行动需要 由类(class)去实现(implement)
